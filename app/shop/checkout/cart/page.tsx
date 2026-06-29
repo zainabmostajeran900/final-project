0bb45959a9f329c421ab3fe050280ae6bb8eb8bf
zainabmostajeran900 <zeinabmostajeran586@gmail.com>
2026-06-29 14:03:22 +0330

    add log for path

--- app/shop/checkout/cart/page.tsx
+++ app/shop/checkout/cart/page.tsx
@@ -47,51 +47,52 @@ const ShoppingCart: React.FC = () => {
                 سبد خرید شما خالی است.
               </p>
             ) : (
-              cart.map((item) => (
-                <div
-                  key={item._id}
-                  className="flex flex-col md:flex-row items-center gap-6 md:gap-8 border-b py-4 justify-between"
-                >
-                  <Image
-                    className="p-2 object-cover  md:w-[150px] h-[150px] rounded-lg"
-                    src={`https://backend-final-rho.vercel.app/images/products/images/${item.images[0]}`}
-                    width={200}
-                    height={300}
-                    alt={item.name}
-                  />
-
-                  <div className="flex-grow text-center md:text-right space-y-3 text-textColor">
-                    <p className="font-semibold text-lg text-textColor md:text-2xl">
-                      {item.name}
-                    </p>
-                    <p className="text-lg md:text-textColor">
-                      {item.price.toLocaleString("ar-EG")} تومان
-                    </p>
-                  </div>
-
-                  <div className="flex items-center gap-8 w-full sm:w-auto justify-center">
-                    <input
-                      type="number"
-                      min="1"
-                      value={item.cartQuantity}
-                      onChange={(e) =>
-                        handleQuantityChange(item._id, Number(e.target.value))
-                      }
-                      className="w-16 md:w-16 border border-gray-300 rounded-md text-center text-sm md:text-base"
+              cart.map((item) => {
+                console.log("image name:", item.images[0]);
+                return (
+                  <div
+                    key={item._id}
+                    className="flex flex-col md:flex-row items-center gap-6 md:gap-8 border-b py-4 justify-between"
+                  >
+                    <Image
+                      className="p-2 object-cover  md:w-[150px] h-[150px] rounded-lg"
+                      src={`https://backend-final-rho.vercel.app/images/products/images/${item.images[0]}`}
+                      width={200}
+                      height={300}
+                      alt={item.name}
                     />
-                    <button
-                      onClick={() => handleRemoveItem(item._id)}
-                      className="text-textColor size-8"
-                    >
-                      <FaTrash />
-                    </button>
+                    <div className="flex-grow text-center md:text-right space-y-3 text-textColor">
+                      <p className="font-semibold text-lg text-textColor md:text-2xl">
+                        {item.name}
+                      </p>
+                      <p className="text-lg md:text-textColor">
+                        {item.price.toLocaleString("ar-EG")} تومان
+                      </p>
+                    </div>
+
+                    <div className="flex items-center gap-8 w-full sm:w-auto justify-center">
+                      <input
+                        type="number"
+                        min="1"
+                        value={item.cartQuantity}
+                        onChange={(e) =>
+                          handleQuantityChange(item._id, Number(e.target.value))
+                        }
+                        className="w-16 md:w-16 border border-gray-300 rounded-md text-center text-sm md:text-base"
+                      />
+                      <button
+                        onClick={() => handleRemoveItem(item._id)}
+                        className="text-textColor size-8"
+                      >
+                        <FaTrash />
+                      </button>
+                    </div>
                   </div>
-                </div>
-              ))
+                );
+              })
             )}
           </div>
         </div>
-
         <div className="flex flex-col md:flex-row items-center justify-between gap-4">
           <div className="flex items-center gap-2">
             <p className="font-bold text-sm md:text-gray-800">جمع کل :</p>
